fbd1c8a06d159430e95629d805d44f3c1e467744
stealthscripter <nicolasayele@gmail.com>
2024-06-21 20:10:06 +0300

    second

--- app/src/main/java/com/example/attendance/MainActivity.java
+++ app/src/main/java/com/example/attendance/MainActivity.java
@@ -10,15 +10,20 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+import androidx.recyclerview.widget.RecyclerView;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 public class MainActivity extends AppCompatActivity {
 
+    FloatingActionButton fab;
+    RecyclerView recyclerView;
+
+    ClassAdapter classAdapter;
+    RecyclerView.LayoutManager layoutManager;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
 
-        FloatingActionButton fab;
 
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
